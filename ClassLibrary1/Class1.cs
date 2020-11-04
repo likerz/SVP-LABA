747485e5bbefbe2d335225a49a8caeb3115b3aed
likerz <mrsocolov95@mail.ru>
2020-11-04 15:14:22 +0300

    Добавил окно в котором будет произвадится расчет

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class MUDA
+    {
+        private string a;
+        private string b;
+
+        public MUDA(string a, string b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public string sum()
+        {
+            return " = " + ((Int32.Parse(a)) * (Int32.Parse(a)) * (Int32.Parse(a)) + 3 * (Int32.Parse(b)) * (Int32.Parse(a)) * (Int32.Parse(a)) + 3 * (Int32.Parse(b)) * (Int32.Parse(b)) * (Int32.Parse(a)) + (Int32.Parse(b)) * (Int32.Parse(b)) * (Int32.Parse(b)));
+        }
+    }
+
+}
+
+
---
+++ WpfApp4/Window1.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ClassLibrary1;
+using System.IO;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MUDA s = new MUDA(TBB.Text, TBA.Text);
+            lbl.Content = "Summ" + s.sum();
+            StreamWriter SaveRes = new StreamWriter(@"D:\Programms\WpfApp4\RES.txt", true);
+            SaveRes.WriteLine(s.sum());
+            SaveRes.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Hide();
+            MainWindow menu = new MainWindow();
+            menu.Show();
+            Close();
+        }
+    }
+}
